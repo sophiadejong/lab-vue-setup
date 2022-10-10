d3376d1d442c93ffd98f6e2224832d009e4804ee
Sophia de Jong <Sophia@Sophia-Marina-de-Jong-3.local>
2022-10-10 15:47:18 +0200

    iteration2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,48 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{message2}}</h2>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    message2: String,
+  },
+  data() {
+    return {
+      message2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quidem.',
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.read-the-docs {
-  color: #888;
+h3, h1 {
+  margin: 40px 0 0;
+  color: pink;
 }
+
+h2 {
+  padding: 20px;
+  font-style: oblique;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
 </style>
+
---
+++ src/components/Iteration1.vue
@@ -0,0 +1,29 @@
+<template>
+    <img :src="imagen" />
+    <div :class="classes">Hola Mundo</div>
+    <div :style="{backgroundColor: 'blue', color: 'red'}">Hola Mundo</div>
+ 
+ </template>
+ <script setup>
+ 
+ const imagen = 'https://1000marcas.net/wp-content/uploads/2020/02/logo-Google.png';
+ const classes = ['text', 'color', 'bg']
+ 
+ </script>
+ <style scoped>
+ .text {
+     font-size: 20px;
+ }
+ 
+ .color {
+     color: pink;
+ }
+ 
+ .bg {
+     background-color: red;
+ }
+ img  {
+     width: 20vw;
+ }
+ 
+ </style>
